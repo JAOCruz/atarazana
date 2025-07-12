5f0f137bbf6eeb3803d32f02cbcb72436d88c8e1
JAOCruz <notmeee00@gmail.com>
2025-07-12 11:15:56 -0400

    Mobile version burger menu

--- public/js/custom.js
+++ public/js/custom.js
@@ -78,15 +78,84 @@ $(document).ready(function()
 
 	function initMenu()
 	{
-		if($('.menu').length)
+		// Initialize the mobile menu from header.html
+		var hamburgerBtn = $('#hamburgerBtn');
+		var mobileMenuOverlay = $('#mobileMenuOverlay');
+		
+		if(hamburgerBtn.length && mobileMenuOverlay.length)
+		{
+			hamburgerBtn.on('click', function()
+			{
+				hamburgerBtn.toggleClass('active');
+				mobileMenuOverlay.toggleClass('active');
+				if(mobileMenuOverlay.hasClass('active')) {
+					document.body.style.overflow = 'hidden';
+				} else {
+					document.body.style.overflow = '';
+				}
+			});
+			
+			// Close button functionality
+			$('#mobileMenuClose').on('click', function() {
+				hamburgerBtn.removeClass('active');
+				mobileMenuOverlay.removeClass('active');
+				document.body.style.overflow = '';
+			});
+			
+			// Close when clicking menu links
+			$('.mobile_menu_nav a').on('click', function() {
+				setTimeout(function() {
+					hamburgerBtn.removeClass('active');
+					mobileMenuOverlay.removeClass('active');
+					document.body.style.overflow = '';
+				}, 100);
+			});
+			
+			// Close when clicking outside menu content
+			mobileMenuOverlay.on('click', function(e) {
+				if(e.target === mobileMenuOverlay[0]) {
+					hamburgerBtn.removeClass('active');
+					mobileMenuOverlay.removeClass('active');
+					document.body.style.overflow = '';
+				}
+			});
+			
+			// Mobile dropdown functionality for "Eventos"
+			$('.mobile-dropdown-title').on('click', function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+				$(this).toggleClass('active');
+				$(this).next('.mobile-dropdown-menu').toggleClass('show');
+			});
+		}
+		// Legacy menu support for other pages if needed
+		else if($('.menu').length && $('.hamburger').not('#hamburgerBtn').length)
 		{
 			var menu = $('.menu');
-			hamburger.on('click', function()
+			var oldHamburger = $('.hamburger').not('#hamburgerBtn');
+			
+			oldHamburger.on('click', function()
 			{
-				hamburger.toggleClass('active');
+				oldHamburger.toggleClass('active');
 				menu.toggleClass('active');
 			});
 		}
+		
+		// Desktop dropdown functionality
+		$('.dropdown-toggle').on('click', function(e) {
+			e.preventDefault();
+			$(this).next('.dropdown-menu').toggleClass('show');
+		});
+		
+		// Close desktop dropdown when clicking outside
+		$(document).on('click', function(e) {
+			var dropdownToggle = $('.dropdown-toggle');
+			var dropdownMenu = $('.dropdown-menu');
+			if (!dropdownToggle.is(e.target) && dropdownToggle.has(e.target).length === 0 && 
+				!dropdownMenu.is(e.target) && dropdownMenu.has(e.target).length === 0) {
+				dropdownMenu.removeClass('show');
+			}
+		});
 	}
 
 	/* 
